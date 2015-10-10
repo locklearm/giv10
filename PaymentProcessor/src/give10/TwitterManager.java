73c497a73f6c6a97a888a982a885cef064406682
Martin Locklear <locklear.martin@gmail.com>
2015-10-10 13:24:58 -0500

    Added the libraries to the payment processing project.

--- PaymentProcessor/src/give10/TwitterManager.java
+++ PaymentProcessor/src/give10/TwitterManager.java
@@ -3,6 +3,10 @@
  */
 package give10;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 /**
  * @author Martin Locklear (locklear.martin@gmail.com)
  *
@@ -15,6 +19,22 @@ public class TwitterManager {
 	public static void main(String[] args) {
 		
 		System.out.println("Yo.");
+		
+		//Set up credentials
+//		ConfigurationBuilder cb = new ConfigurationBuilder();
+		
+		
+		
+		
+//		Twitter twitter = TwitterFactory.getSingleton();
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Yep.");
 
 	}
 
