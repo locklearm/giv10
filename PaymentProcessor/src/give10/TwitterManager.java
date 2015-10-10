8423a17702ada8f24800a330f9c59c690ab3d54b
Martin Locklear <locklear.martin@gmail.com>
2015-10-10 13:15:13 -0500

    Initial Commit.  Setup the project, etc.

---
+++ PaymentProcessor/src/give10/TwitterManager.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package give10;
+
+/**
+ * @author Martin Locklear (locklear.martin@gmail.com)
+ *
+ */
+public class TwitterManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Yo.");
+
+	}
+
+}
